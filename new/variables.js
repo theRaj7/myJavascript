22a6f86eb28426626611daf4cebc874ca347db46
theRaj7 <122991369+theRaj7@users.noreply.github.com>
2024-06-29 15:43:59 +0000

    variable notes

---
+++ new/variables.js
@@ -0,0 +1,15 @@
+/*
+just imagine you have to create a form and whant to store the data 
+there have must some options like accountId,password, city, emailid
+*/
+const accountId = 75583 //const means constant . this value cannot be change 
+let accountEmail = "raj@gmail.com"  //we can change the value as per our requirement.
+var accountPassword = "53408"   //not to use var because it has some function blocks issue.
+accountCity = "ambernath"      //this is only for understanding . prefer to not use this.
+
+//let is perfect
+
+let accountState
+
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
