416613934d3445d1335c91ffb939e67df2b42fda
theRaj7 <122991369+theRaj7@users.noreply.github.com>
2024-07-04 06:18:35 +0000

    new

--- new/02_datatype.js
+++ new/02_datatype.js
@@ -2,3 +2,14 @@
 
 //alert(3*3) //we are using node js , not browser 
 
+console.log("myJAVASCRIPt")
+
+/*
+basic datatypes
+number => 2 to power 53
+string => ""(use double qoutes mostly)
+boolean => true/false
+null => standalone value
+undefined
+
+*/
