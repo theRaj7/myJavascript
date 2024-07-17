0e7e1db99186279e008898a09f41706649a0d190
theRaj7 <122991369+theRaj7@users.noreply.github.com>
2024-07-17 07:40:48 +0000

    datatype

---
+++ new/datatype-summury.js
@@ -0,0 +1,23 @@
+
+// There are two datatypes:
+/*   1. Primitive 
+    there are 7 types of it:
+        String , Number , Boolean , null , undefined , symbol , BigInt
+we know the all the examples of primitive Datatype
+*/
+/*
+    2. Referance(Non Primitive)
+        Array , Objects , Function 
+*/
+const myTea = ["Milk","Adrak","Chaipatti","Water"];
+
+let myObject = {
+    Milk : "1/2 Liter",
+    water : "According TO milk"
+}
+
+const myfunction = function(){
+    console.log("Hello World");
+
+}
+console.log(myObject);
