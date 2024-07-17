d87898e2fe78d2e2835f851e3d69b3494937fbae
theRaj7 <122991369+theRaj7@users.noreply.github.com>
2024-07-17 06:30:40 +0000

    new

--- new/04_Comparisons.js
+++ new/04_Comparisons.js
@@ -1,6 +1,39 @@
-console.log(2 = 2);
-console.log(2 < 2);
-console.log(2 > 2);
-console.log(2 != 2);
+// console.log(2 == 2);
+// console.log(2 < 2);
+// console.log(2 > 2);
+// console.log(2 != 2);
+// This are simple comparisons beacuse it have same datatype
+
+
+// console.log("7" == 7);
+// console.log("7" < 5);
+// console.log("7" > 5);
+// console.log("7" != 5);
+// console.log("7" != "5");
+// console.log("7" != "7");
+// this are little complex becuse, here datatype is different from each other and for string they have to convert into number for comparions
+
+
+
+// console.log(null == 0);
+// console.log(null < 0);
+// console.log(null > 0);
+
+// console.log(undefined == 0);
+// console.log(undefined < 0);
+// console.log(undefined > 0);
+// Avoid Such Comparisons Because they are so complex and not easy to understand 
+
+
+// ===
+// this are called as strict comparison 
+
+console.log("2" === 2);
+
+
+
+
+
+
 
 
