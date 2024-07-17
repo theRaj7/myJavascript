e478084ab9c575ac152b5b1f26d79e7f3f1dc6ad
theRaj7 <122991369+theRaj7@users.noreply.github.com>
2024-07-17 06:18:31 +0000

    new

---
+++ new/03_conversionOperation.js
@@ -0,0 +1,11 @@
+let someString  = "Heeqw"
+
+let numberString = Number(someString)
+// console.log(numberString);
+// console.log(typeof numberString);
+
+let someNumber = "77"
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
